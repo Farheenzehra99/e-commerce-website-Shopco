4c8b66d6d8fa9498e4bfd7e7e3a35399bf9add27
Farheenzehra99 <farheenzehra481@gmail.com>
2024-10-10 15:56:53 +0500

    project completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Brands from "@/components/homepage/Brands";
 import DressStyle from "@/components/homepage/DressStyle";
 import Header from "@/components/homepage/Header";
 import Reviews from "@/components/homepage/Reviews";
-import { Product } from "@/types/product.types";
+import { Product } from "@/types/product.type";
 import { Review } from "@/types/review.types";
 
 export const newArrivalsData: Product[] = [
@@ -225,39 +225,6 @@ export default function Home() {
             data={topSellingData}
             viewAllLink="/shop#top-selling"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
         </div>
         <div className="mb-[50px] sm:mb-20">
           <DressStyle />
